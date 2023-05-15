b35c62805493a1a7789ba4c65a4c770b07045443
aarjav123cam <aarjav.jain@gmail.com>
2023-05-15 22:54:31 +0100

    added projects

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,10 @@ function About() {
   return (
     <div>
       <h2>About Me</h2>
-      <p>Hello! I'm a passionate Theoretical Physics Masters graduate from the University of Cambridge who has found his calling in the field of Software Engineering. In my current role at THG, I leverage my skills in Python, Java, JavaScript, GCP, and Docker to solve complex problems.</p>
-      <p>Being a scientist at heart, I'm intrigued by the application of Mathematics and Machine Learning in real-world scenarios. I strive to bring this curiosity and rigorous analytical thinking to my work as a software engineer. Beyond work, you will often find me engrossed in a cricket match or pushing my limits in a marathon.</p>
+      <p>Hello! I'm a Theoretical Physics Masters graduate from the University of Cambridge, working in Software Engineering. In my current role at THG, I apply my skills in Python, Machine Learning, GCP, and Docker to solve complex problems in ML and ML Ops.</p>
+      <p>Being a scientist at heart, I'm intrigued by the application of Mathematics and Machine Learning in real-world scenarios. I strive to bring this curiosity and rigorous analytical thinking to my work in tech. </p>
+      <p>I'm always on the lookout for interesting problems and projects. Recently, I have become interested in finance and markets. I am also keen on building full-stack software projects to keep my skills sharp.</p>
+      <p>Beyond work, you will often find me watching cricket (or dubiously playing), or out jogging.</p>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import torus from '../images/torus.png';
 
-function Experience() {
+function Projects() {
   return (
     <div>
-      <h2>Experience</h2>
-      <p>Over the years, I've had the opportunity to work on some intriguing projects and roles:</p>
-      <ul>
-        <li><strong>Machine Learning Engineer, THG:</strong> As part of the ML team, I deployed a SpellChecker model API to GCP Cloud run.</li>
-        <li><strong>Software Engineer, THG:</strong> Received extensive software engineering training and led a team to create a sales dashboard using React and Javascript.</li>
-        <li><strong>ML Research Intern, Faraday Institution:</strong> Created surrogate models for thermal runaway of Li-Ion batteries using Gaussian processes, which significantly reduced computation time.</li>
+      <h2>Projects</h2>
+      <div className="project-image" >
+        <img src={torus} alt="torus" className="project-image" />
+        <figcaption className="image-caption">Neural network solution of torus under Ricci flow, embedded into &#8477;&#178;. Coloured lines are some solutions to its geodesics.</figcaption> 
+      </div>
+
+      <p className="project-text">Here are a few projects I've been involved in:</p>
+      <h4>Machine Learning</h4>
+      <ul>  
+        <li><strong>ML solutions Ricci Flow on real metrics:</strong> Using physics-informed neural networks to investigate metric solutions to the geometry problem of Ricci Flow: a curvature-based PDE on metrics which helped prove the famous Poincaré conjecture. Created embeddings of the solutions, and subsequent visualisations. Accepted by a NeurIPS workshop in 2023 and the full paper can be found on <a href="https://arxiv.org/abs/2212.05892">Arxiv</a>.   </li>
+        <li><strong>Drift Detection in ML models:</strong> Investigating the issue of concept and data drift-an important issue to diagnose in production ML models. Trained a toy MNIST CNN model and simulated drift using various noise additions to calculate statistical deviations from which one can create automatic model retraining thresholds. This can help ensure an accurate production model.</li>
+        <li><strong>Transformer Stock Price Prediction Model:</strong> Investigated the efficacy of transformer models for stock price prediction. By creating sequences of price data, created trading signals for various stock tickers, with mild success. </li>
+      </ul>
+      <h4>Software Engineering</h4>
+      <ul>  
+        <li><strong>Full stack task list project in Java and React:</strong> </li>
+        <li><strong>Drift Detection in ML models:</strong> Investigating the issue of concept and data drift-an important issue to diagnose in production ML models. Trained a toy MNIST CNN model and simulated drift using various noise additions to calculate statistical deviations from which one can create automatic model retraining thresholds. This can help ensure an accurate production model.</li>
+        <li><strong>Transformer Stock Price Prediction Model:</strong> Investigated the efficacy of transformer models for stock price prediction. By creating sequences of price data, created trading signals for various stock tickers, with mild success. </li>
+      </ul>
+      <h4>Physics</h4>
+      <ul>  
+      <li><strong>Modelling n-body Orbit:</strong> Wrote a Python script to model n-body orbit using numerical integration, testing solution stability and sensitivity to initial conditions.</li>
+
       </ul>
     </div>
   );
 }
 
-export default Experience;
+export default Projects;
+
+
+// <li><strong>Digit Recognizer:</strong> Trained a TensorFlow model with a Convolutional Neural Network to recognize digits from the MNIST database.</li>
+//         <li><strong>Housing Price Predictor:</strong> Developed a model using Random Forest and ensemble methods to predict housing prices.</li>
+//         <li><strong>Titanic Survival Predictor:</strong> Used a Support Vector Classifier and data visualization techniques to predict the survival of passengers on the Titanic.</li>
+//         <li><strong>Modelling n-body Orbit:</strong> Wrote a Python script to model n-body orbit using numerical integration, testing solution stability and sensitivity to initial conditions.</li>
